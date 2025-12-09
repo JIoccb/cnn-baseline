3e3625fe732273f3b5cf0f2fce090414c0671f5e
Artyom Rashevsky <albatroz3000@yandex.ru>
2025-12-09 18:09:20 +0300

    del

--- Makefile
+++
@@ -1,8 +0,0 @@
-train:
-	PYTHONPATH=. python src/train.py
-
-test:
-	PYTHONPATH=. python src/test.py
-
-tensorboard:
-	tensorboard --logdir /home/student03/work/Artyom/work_dir/experiments/artyom_perspective_cnn/log_dir
--- src/modules/metrics.py
+++
@@ -1,9 +0,0 @@
-from torchmetrics import MetricCollection
-from torchmetrics.regression import MeanAbsoluteError, MeanAbsolutePercentageError, MeanSquaredError
-
-def get_metrics(**kwargs) -> MetricCollection:
-    return MetricCollection({
-        'mae': MeanAbsoluteError(**kwargs),
-        'mape': MeanAbsolutePercentageError(**kwargs),
-        'mse': MeanSquaredError(**kwargs)
-    })
